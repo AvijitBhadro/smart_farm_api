f99eb6b998fe8472fcb9eb7e1fee07bcf9dc4653
AvijitBhadro <abhadro621@gmail.com>
2026-06-05 09:14:46 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# store latest sensor data
+data_store = {
+    "temperature": 0.0,
+    "humidity": 0.0
+    # "soil": 0,
+    # "light": 0   # NEW
+}
+
+# data model
+class SensorData(BaseModel):
+    temperature: float
+    humidity: float
+    # soil: int
+    # light: int   # NEW
+
+@app.post("/data")
+def receive_data(data: SensorData):
+    data_store["temperature"] = data.temperature
+    data_store["humidity"] = data.humidity
+    # data_store["soil"] = data.soil
+    # data_store["light"] = data.light  # NEW
+    return {"status": "ok"}
+
+@app.get("/api")
+def get_data():
+    return data_store
